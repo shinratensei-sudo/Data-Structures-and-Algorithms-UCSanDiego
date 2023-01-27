c021563b94236428fd01ed70ed9c93f8df941213
shinratensei-sudo <112606105+shinratensei-sudo@users.noreply.github.com>
2023-01-26 20:23:59 -0600

    Update fibonacciList.java
    Comments added.

--- Week2/fibonacciList.java
+++ Week2/fibonacciList.java
@@ -6,8 +6,11 @@ public class fibonacciList {
 
 
         Scanner inputObject = new Scanner(System.in);
+        
+        //read the position of the number that user wants to find
         int inputUser = inputObject.nextInt();
-
+        
+        //get the array until the nth term of the fibonacci sequence. To do this, we call the function fibonacci and uses the input of user as parameter
         long[] result = fibonacci(inputUser);
         System.out.println(Arrays.toString(result));
 
@@ -15,12 +18,14 @@ public class fibonacciList {
     }
 
     public static long [] fibonacci(int k) {
+        //create an array of the length that user wants
         long[] arr;
         arr = new long[k];
-
+        //the first two elements are 0 and 1
         arr[0] = 0L;
         arr[1] = 1L;
-
+           
+        //from 2nd index in the array, get the next one by summing the two previous ones 
         for(int i = 2; i<k; i+=1){
             arr[i] = arr[(i-1)] + arr[(i-2)];
         }
